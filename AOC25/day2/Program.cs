59d787527a76d8d94feca0b3a8c31e8cdf42910e
Adomas <adomas@stalas.lt>
2025-12-01 15:35:54 +0200

    cleanup file structure

--- AOC25/day2/Program.cs
+++ AOC25/day2/Program.cs
@@ -1,3 +1,16 @@
-﻿
+﻿string baseDir = Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName + "\\";
+string[] z = File.ReadAllLines(baseDir + "input.txt");
 
-Console.WriteLine("Dve");
+/// https://adventofcode.com/2025/day/1https://adventofcode.com/2025/day/1
+/// Normal
+{
+    foreach (string line in z)
+    {
+
+    }
+}
+
+///Golfed char count:
+{
+
+}
