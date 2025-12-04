4bee3f13cd983a774ab04334eb125c9c60555a9f
Adomas <adomas@stalas.lt>
2025-12-04 18:42:45 +0200

    day 4 linqed

--- AOC25/day4/Program.cs
+++ AOC25/day4/Program.cs
@@ -1,5 +1,7 @@
 ﻿// https://adventofcode.com/2025/day/4
 
+using System.Linq;
+
 namespace day_4
 {
     public class Program
@@ -8,7 +10,7 @@ namespace day_4
         {
             string[] z = File.ReadAllLines(Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName + "\\input.txt");
             // Normal
-            {                
+            {
                 int sum = 0;
                 List<(int, int)> moveable = [];
                 do
@@ -42,14 +44,44 @@ namespace day_4
                         z[coords.Item1] = z[coords.Item1][..coords.Item2] + '.' + z[coords.Item1][(coords.Item2 + 1)..];
                     }
 
-                } while(moveable.Count > 0);
+                } while (moveable.Count > 0);
 
                 Console.WriteLine(sum);
             }
 
             // pure LINQed: 
             {
-                
+                z = File.ReadAllLines(Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName + "\\input.txt");
+
+                Console.WriteLine(Enumerable.Range(0, 100)
+                .Aggregate(
+                    (sum: 0, z),
+                    (acc, _) => Enumerable.Range(0, acc.z.Length)
+                        .SelectMany(i => Enumerable.Range(0, acc.z[i].Length)
+                            .Where(j => Enumerable.Range(-1, 3)
+                                .SelectMany(x => Enumerable.Range(-1, 3)
+                                    .Where(y => x != 0 || y != 0)
+                                    .Select(y => (x, y)))
+                                .Where(t =>
+                                    i + t.y >= 0 && i + t.y < acc.z.Length &&
+                                    j + t.x >= 0 && j + t.x < acc.z[i + t.y].Length &&
+                                    acc.z[i + t.y][j + t.x] == '@')
+                                .Count() < 4 && acc.z[i][j] == '@')
+                            .Select(j => (i, j))
+                        )
+                        .ToList().Aggregate(
+                        acc,
+                        (state, coords) => (
+                            sum: state.sum + 1,
+                            z: state.z.Select((row, i) =>
+                                i == coords.i
+                                    ? row[..coords.j] + '.' + row[(coords.j + 1)..]
+                                    : row
+                            ).ToArray()
+                        )
+                    )
+                ).sum);
+                //Console.WriteLine(Enumerable.Range(0,100).Aggregate((sum:0,z),(acc,_)=>Enumerable.Range(0,acc.z.Length).SelectMany(i=>Enumerable.Range(0,acc.z[i].Length).Where(j=>Enumerable.Range(-1,3).SelectMany(x=>Enumerable.Range(-1,3).Where(y=>x!=0||y!=0).Select(y=>(x,y))).Where(t=>i+t.y>=0&&i+t.y<acc.z.Length&&j+t.x>=0&&j+t.x<acc.z[i+t.y].Length&&acc.z[i+t.y][j+t.x]=='@').Count()<4&&acc.z[i][j]=='@').Select(j=>(i,j))).ToList().Aggregate(acc,(state,coords)=>(sum:state.sum+1,z:state.z.Select((row,i)=>i==coords.i?row[..coords.j]+'.'+row[(coords.j+1)..]:row).ToArray()))).sum);
             }
         }
     }
