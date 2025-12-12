af2f837d9465df2bbff64890d27e48a7ec94aa39
Adomas <adomas@stalas.lt>
2025-12-12 23:49:53 +0200

    day 11

--- AOC25/day10/Program.cs
+++ AOC25/day10/Program.cs
@@ -6,16 +6,23 @@ namespace day_10
     {
         internal static void Main()
         {
-            string[] z = File.ReadAllLines(Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName + "\\input.txt");
+            string[] z = File.ReadAllLines(Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName + "\\test.txt");
             // Normal
             {
                 List<List<int>> goals = z.Select(l => l.Split(" ")[0][1..^1].Select(c => c == '#' ? 1 : 0).ToList()).ToList();
                 List<List<int>> joltages = z.Select(l => l.Split(" ")[^1][1..^1].Split(",").Select(int.Parse).ToList()).ToList();
                 List<List<List<int>>> combos = z.Select(l => l.Split(" ")[1..^1].Select(n => n[1..^1].Split(",").Select(int.Parse).ToList()).ToList()).ToList();
+                List<List<List<int>>> joltageComboList = [];
+                List<List<int>> joltageComboListSub = [];
+                List<int> sequences = [];
 
                 int sum = 0;
                 for (int i = 0; i < z.Length; i++)
                 {
+                    sequences = [];
+                    tryNewCombo(i);
+                    joltageComboList.Add(joltageComboListSub);
+
                     int lowest = combos[i].Count;
                     for (int j = 0; j < Math.Pow(2, combos[i].Count); j++)
                     {
@@ -53,12 +60,69 @@ namespace day_10
                     sum += lowest;
                 }
                 Console.WriteLine(sum);
+
+                //dp
+                void tryNewCombo(int i)
+                {
+                    //base case
+                    if (sequences.Count == combos[i].Count)
+                    {
+                        if(isJoltageCorrect(sequences, i) == 0)
+                        {
+                            joltageComboListSub.Add(sequences);
+                        }
+                        sequences.RemoveAt(sequences.Count - 1);
+                    }
+                    else
+                    {
+                        int maxJolt = 0;
+                        for (int j = 0; j < combos[i][sequences.Count].Count; j++)
+                        {
+                            if (joltages[i][combos[i][sequences.Count][j]] > maxJolt)
+                                maxJolt = joltages[i][combos[i][sequences.Count][j]];
+                        }
+                        bool ovf = false;
+                        for (int j = 0; j < maxJolt && !ovf; j++)
+                        {
+                            sequences.Add(j);
+                            if (isJoltageCorrect(sequences, i) != 1)
+                            {
+                                tryNewCombo(i);
+                            }
+                            else
+                            {
+                                sequences.RemoveAt(sequences.Count - 1);
+                            }
+                        }
+                    }
+                }
+                int isJoltageCorrect(List<int> sequences, int i)
+                {
+                    List<int> joltList = Enumerable.Range(0, joltages[i].Count).ToList();
+                    for (int j = 0; j < sequences.Count; j++)
+                    {
+                        for (int k = 0; k < combos[i][j].Count; k++)
+                        {
+                            joltList[combos[i][j][k]] += sequences[j];
+                            if (joltList[combos[i][j][k]] > joltages[i][k])
+                                return 1;
+                        }
+                    }
+                    for (int j = 0; j < joltages[i].Count; j++)
+                    {
+                        if (joltList[j] != joltages[i][j])
+                            return -1;
+                    }
+                    return 0;
+                }
             }
 
             //
             {
 
             }
+
+            
         }
     }
 }
---
+++ AOC25/day11/Program.cs
@@ -0,0 +1,65 @@
+﻿// https://adventofcode.com/2025/day/11
+
+namespace day_11
+{
+    public class Program
+    {
+
+        internal static void Main()
+        {
+            string[] z = File.ReadAllLines(Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName + "\\input.txt");
+            // Normal
+            {
+                List<string> ind = z.Select(l => l.Split(" ")[0][..^1]).ToList();
+                List<Node> nodes = [];
+                for (int i = 0; i < z.Length; i++)
+                {
+                    nodes.Add(new Node { val = ind[i], conns = z[i].Split(" ")[1..].ToList() });
+                }
+                
+                Console.WriteLine(CountSpecial(nodes.First(n => n.val == "svr"), [], nodes));
+            }
+        }
+
+        //dp black magic
+        static Dictionary<(string, bool, bool), long> memo = [];
+        static long CountSpecial(Node n, HashSet<string> visiting, List<Node> nodes)
+        {
+            bool h1 = n.val == "dac" || visiting.Contains("dac");
+            bool h2 = n.val == "fft" || visiting.Contains("fft");
+
+            var key = (n.val, h1, h2);
+            if (memo.TryGetValue(key, out var v)) 
+                return v;
+
+            if (!visiting.Add(n.val)) 
+                return 0;
+
+            long sum = 0;
+
+            foreach (string c in n.conns)
+            {
+                if (c == "out")
+                {
+                    if (h1 && h2) 
+                        sum++;
+                }
+                else
+                {
+                    sum += CountSpecial(nodes.Find(x => x.val == c), visiting, nodes);
+                }
+            }
+
+            visiting.Remove(n.val);
+            memo[key] = sum;
+            return sum;
+        }
+
+
+        public struct Node
+        {
+            public string val;
+            public List<string> conns;
+        }
+    }
+}
